8304e6e43362683dbf7e109ce71f183d2d706dae
Fazlul Kabir <fazlulkabir34@gmail.com>
2020-07-02 00:23:09 +0600

    mend

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        $this->call(RoleAndPermissionTableSeeder::class);
-       $this->call(UsersTableSeeder::class);
+        // $this->call(RoleAndPermissionTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
